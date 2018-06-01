fb1450c64bbb82d21b29d6c5f8fe53edd37526bb
Stephen <wizebin@gmail.com>
2018-06-01 00:31:50 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { wrapAction } from 'nexusdk';
+import fs from 'fs';
+import path from 'path';
+
+function assureDirExists(targetDir, { relativeToSource = false } = {}) {
+  const normalizedDir = path.normalize(targetDir);
+  const pathSeparator = path.sep;
+  const initialDir = path.isAbsolute(targetDir) ? pathSeparator : '';
+  const baseDir = relativeToSource ? __dirname : '.';
+
+  targetDir.split(pathSeparator).reduce((parentDir, childDir) => {
+    const currentDir = path.resolve(baseDir, parentDir, childDir);
+    if (!fs.existsSync(currentDir)) {
+      fs.mkdirSync(currentDir);
+    }
+
+    return currentDir;
+  }, initialDir);
+}
+
+wrapAction((properties, sendMessage) => {
+  return new Promise(resolve => {
+    const { filename, data, encoding = 'json', append = false } = properties;
+    assureDirExists(path.dirname(filename));
+    return fs.writeFile(filename, data);
+  });
+})
